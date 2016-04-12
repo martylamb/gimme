cd165bc857acd215720ee6e2fa6c551fe3034d73
Marty Lamb <mlamb@rajant.com>
2016-04-12 18:07:24 -0400

    basic functionality and tests

--- src/main/java/com/rajant/oss/gimme/Gimme.java
+++ src/main/java/com/rajant/oss/gimme/Gimme.java
@@ -1,9 +1,102 @@
 package com.rajant.oss.gimme;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 /**
- *
- * @author mlamb
+ * A simple service locator for dependency inversion
  */
 public class Gimme {
     
+    private static final Map<Class<?>, Supplier> _suppliers = new java.util.HashMap<>();
+    
+    /**
+     * Registers a singleton object as the service implementation for one or more interfaces
+     * @param singleton the singleton being registered
+     * @param ifaces the interfaces for which the singleton will be provided
+     * @return the registered singleton
+     */
+    public static <T> T set(T singleton, Class<? super T>... ifaces) {        
+        setSupplier(
+            new ConstantSupplier(requireNonNull(singleton, "implementation")),
+            ifaces);
+        return singleton;
+    }
+
+    /**
+     * Registers a Supplier to provide service implementations for one or more interfaces
+     * @param supplier the Supplier being registered
+     * @param ifaces the interfaces for which the Supplier will provide implementations
+     * @return the registered Supplier
+     */
+    public static <T> Supplier<T> setSupplier(Supplier<T> supplier, Class<? super T>... ifaces) {
+        requireAtLeastOneElementIn(ifaces);
+        requireNonNull(supplier, "supplier");
+        synchronized(_suppliers) {
+            for (Class<? super T> iface : ifaces) {
+                if (iface == null) throw new NullPointerException("null interface specified");
+                if (!iface.isInterface()) throw new IllegalArgumentException("services can only be registered against interfaces:  impl[" + iface + "]");
+                _suppliers.put(iface, supplier);
+            }
+        }
+        return supplier;
+    }
+    
+    /**
+     * Returns an implementation of the specified interface, or null if no implementation is available
+     * @param iface the interface for which an implementation is desired
+     * @return an implementation of the specified interface, or null if no implementation is available
+     */
+    public static <T> T a(Class<? super T> iface) {
+        synchronized(_suppliers) {
+            Supplier<T> supplier = _suppliers.get(iface);
+            return (supplier == null) ? null : supplier.get();
+        }
+    }
+    
+    /**
+     * Exactly equivalent to `a(Class<? super T> iface)`
+     * @see Gimme#a(java.lang.Class) 
+     */
+    public static <T> T an(Class<? super T> iface) { return a(iface); }
+    
+    /**
+     * Convenience method to return an Optional<T> wrapping around a call to {@link Gimme#a(java.lang.Class)}
+     */
+    public static <T> Optional<T> optional(Class<? super T> iface) {
+        return Optional.ofNullable(a(iface));
+    }
+    
+    /**
+     * Returns an implementation of the specified interface, or throws a NotFoundException if no implementation is available
+     * @param iface the interface for which an implementation is desired
+     * @return an implementation of the specified interface
+     * @throws NotFoundException if no implementation is available
+     */
+    public static <T> T orThrow(Class<? super T> iface) throws NotFoundException {
+        T result = a(iface);
+        if (result == null) throw new NotFoundException("implementation not available: " + iface);
+        return result;
+    }
+            
+    private static <T> T requireNonNull(T t, String type) {
+        if (t == null) throw new NullPointerException("failed to register null service " + type);
+        return t;
+    }
+    
+    private static <T> Class<? super T>[] requireAtLeastOneElementIn(Class<? super T>[] ifaces) {
+        if (ifaces == null || ifaces.length == 0) throw new IllegalArgumentException("at least one interface must be specified");        
+        return ifaces;
+    }
+    
+    private static class ConstantSupplier<T> implements Supplier<T> {
+        private final T _t;
+        private ConstantSupplier(T t) { _t = t; }
+        @Override public T get() { return _t; }
+    }
+        
+    public static class NotFoundException extends Exception {
+        private NotFoundException(String msg) { super(msg); }
+    }
 }
---
+++ src/test/java/com/rajant/oss/gimme/GimmeTest.java
@@ -0,0 +1,76 @@
+package com.rajant.oss.gimme;
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.function.Supplier;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mlamb
+ */
+public class GimmeTest {
+    
+
+    @Test public void testRequiresInterface() {
+        try {
+            Gimme.set(5, Integer.class);
+            fail("able to register a class");
+        } catch (IllegalArgumentException expected) {}
+        
+        Gimme.set(5, Serializable.class);
+    }
+    
+    @Test public void noNullInterfaces() {
+        try {
+            Gimme.set("hello", Serializable.class, CharSequence.class, null);
+            fail("able to register a null interface");
+        } catch (NullPointerException expected) {}
+    }
+    
+    @Test public void noNullVarargs() {
+        try {
+            Gimme.set("hello", null);
+            fail("able to register a null vararg");
+        } catch (IllegalArgumentException expected) {}
+    }
+    
+    @Test public void noNullImplementation() {
+        try {
+            Gimme.set(null, CharSequence.class);
+            fail("able to register a null implementation");
+        } catch (NullPointerException expected) {}        
+    }
+    
+    @Test public void noNullSupplier() {        
+        try {
+            Gimme.setSupplier(null, CharSequence.class);
+            fail("able to register a null supplier");
+        } catch (NullPointerException expected) {}        
+    }
+    
+    @Test public void testThrow() {
+        try {
+            Gimme.orThrow(Runnable.class);
+            fail("did not throw");
+        } catch (Gimme.NotFoundException expected) {}        
+    }
+    
+    @Test public void testMultipleGets() throws Gimme.NotFoundException {
+        String s = "Test String";
+        
+        Gimme.set(s, CharSequence.class);
+        
+        assertEquals(s, Gimme.a(CharSequence.class));
+        assertEquals(s, Gimme.an(CharSequence.class));
+        assertEquals(s, Gimme.optional(CharSequence.class).get());
+        assertEquals(s, Gimme.orThrow(CharSequence.class));
+
+        assertNull(Gimme.a(Runnable.class));
+    }
+}
