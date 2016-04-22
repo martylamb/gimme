2a3ae6fcde5fcad31764484d88b3af8c99092106
mlamb <mlamb@rajant.com>
2016-04-22 14:35:14 -0400

    decruft

--- src/test/java/com/rajant/oss/gimme/GimmeTest.java
+++ src/test/java/com/rajant/oss/gimme/GimmeTest.java
@@ -1,12 +1,6 @@
 package com.rajant.oss.gimme;
 
 import java.io.Serializable;
-import java.util.Optional;
-import java.util.function.Supplier;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
